548f533f3d4c22cfe6c4f1b32c96bda493aa68f6
Lizette97 <2122200436@soy.utj.edu.mx>
2025-07-14 23:40:23 -0600

    Create localDB.ts

---
+++ src/utils/localDB.ts
@@ -0,0 +1,32 @@
+import {openDB} from 'idb';
+
+const DB_NAME = 'taskDB';
+const STORE_NAME = 'tasks';
+
+export const dbPromise = openDB(DB_NAME, 1, {
+    upgrade(db){
+        if(!db.objectStoreNames.contains(STORE_NAME)){
+            db.createObjectStore(STORE_NAME,{keyPath: 'id', autoIncrement: true});
+        }
+    },
+
+});
+
+
+//Guardar la tarea en la base de datos local
+ export async function saveTask(task){
+    const db = await dbPromise;
+    return db.put(STORE_NAME, task);
+ }
+
+ //Obtener todas las tareas de la base de datos local
+ export async function getTasks(){
+    const db = await dbPromise;
+    return db.getAll(STORE_NAME);
+ }
+
+ //Eliminar una tarea de la base de datos local
+ export async function deleteTask(id){
+    const db = await dbPromise;
+    return db.delete(STORE_NAME, id);
+ }
